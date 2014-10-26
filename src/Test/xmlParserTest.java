e60fb0c69088d2c9d9298e69e609495d48ee46db
njw374 <njw374@hotmail.com>
2014-10-26 14:28:38 -0700

    First implementation of the XML parser and the Fuser, still using mock objects, still need to add test

---
+++ src/Test/xmlParserTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class xmlParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
