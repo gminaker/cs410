ce717cd54cfcbefe8d2add2d80a22f14dcd1e4d1
njw374 <njw374@hotmail.com>
2014-10-27 17:29:07 -0700

    added comments to junit files explaining testing strategy

--- src/Test/fuserTest.java
+++ src/Test/fuserTest.java
@@ -6,6 +6,14 @@ import org.junit.Test;
 
 public class fuserTest {
 
+//this will be used for integration testing
+//to be implemented in sprint 2
+//main focus of sprint 1 was basic funtionality
+
+//I felt this was lower priority since it would be more
+//usefull once more error handling and input checking capabilities
+//had been implemented
+
 	@Test
 	public void test() {
 		fail("Not yet implemented");
