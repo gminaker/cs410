a2f93f8ccc48acb95531762a44950a6f61b3b6e1
Leo Marchand <lgtmarchand@gmail.com>
2014-11-16 17:26:05 -0800

    Additional parser implementation

--- src/cs410/HTMLParser.java
+++ src/cs410/HTMLParser.java
@@ -9,9 +9,18 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class HTMLParser {
+	
+	
+	public static Object[][]complexityAndClassNamesForFilepaths(String[] filepaths) {
+		Object[][] returnValue = new Object[filepaths.length][];
+		for (int i = 0; i<filepaths.length; i++) {
+			returnValue[i] = new Object[] {filepaths[i], complexityForFilepath(filepaths[i])};
+		}
+		return returnValue;
+	}
 
 	
-	public static double complexityForFilepath(String filepath) {
+	private static double complexityForFilepath(String filepath) {
 		double complexity = 0;
 		String htmlFileName = filepath.replaceAll("\\.\\w+", ".html");
 		htmlFileName.replace('/', '.');	
--- src/visualization/visualizer.java
+++ src/visualization/visualizer.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 import processing.core.PGraphics;
+import cs410.HTMLParser;
 import cs410.fuser;
+import cs410.xmlParser;
 
 //Draws a graph given the parent node
 public class visualizer extends PApplet {
@@ -38,7 +40,9 @@ public class visualizer extends PApplet {
 	public void setup() {
 		size(SIZE_WIDTH, SIZE_HEIGHT);
 		pg = createGraphics(SIZE_WIDTH, SIZE_HEIGHT);
-
+		xmlParser xParser = new xmlParser();
+		String[] filepaths = new String[4];
+		Object[][] htmlParserOutput = HTMLParser.complexityAndClassNamesForFilepaths(filepaths);
 		fuser resultFuser = new fuser();
 		Node drawGraph = resultFuser.fuse();
 		graph = drawGraph;
