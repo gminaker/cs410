3912971c9ee291b5d6b604501efcc2f5fc144005
imtiana <imjihyung@gmail.com>
2014-11-20 15:36:33 -0800

    add git parser test

--- src/Test/gitInspectorXMLParserTest.java
+++ src/Test/gitInspectorXMLParserTest.java
@@ -3,6 +3,9 @@ package Test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -21,68 +24,154 @@ import cs410.gitInspectorParser;
 public class gitInspectorXMLParserTest {
 
 	private static gitInspectorParser GitInspectorParser;
-	
+
 	private static String filePath = "C:/Git/cs410/src/Test/TestGitInspectorOutput.xml";
-	private static int testMaxFileNum = GitInspectorParser.maxFileNum;
-	private static int testMaxauthorNum = GitInspectorParser.maxAuthorNum;
-	
+
+	private static int expectedMaxFileNum = gitInspectorParser.maxFileNum;
+	private static int expectedMaxauthorNum = gitInspectorParser.maxAuthorNum;
+	private static int expectedFilePathsNum = 35; // based on
+	// TestGitInspectorOutput.xml
+
+	private ArrayList<String> expectedAllAuthorNames = new ArrayList<String>();
+	private ArrayList<String> expectedTop6AuthorNames = new ArrayList<String>();
+
 	private static Document testDoc;
 	private static XPath testXPath;
-	
-	static Object[][] testOutputArray = new Object[testMaxFileNum][testMaxauthorNum+1];	
-        
+
+	static Object[][] testOutputArray = new Object[expectedMaxFileNum][expectedMaxauthorNum + 1];
+
 	@Test
-	// Output array is not null and correctly formatted as 6 x 7 
+	// Output array is not null and correctly formatted as an 6 x 7 array
 	public void returnParsedArray_OutputArrayFormat_HappyPath() {
 		try {
 			testOutputArray = gitInspectorParser.returnParsedArray(filePath);
 			Assert.assertNotNull(testOutputArray);
-			
-			Assert.assertEquals(testMaxFileNum, testOutputArray.length);
+
+			Assert.assertEquals(expectedMaxFileNum, testOutputArray.length);
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 	}
 
 	@Test
-	public void returnParsedArray_GetAuthors_HappyPath() throws ParserConfigurationException, SAXException, IOException {
-		
-		setUpDocument();
-		
-    	TreeMap<String, Double> AuthorTable = new TreeMap<String, Double>();
-    	AuthorTable = gitInspectorParser.getAuthors(testDoc, testXPath);
-    	
-    	Assert.assertEquals(AuthorTable.size(), 8);
-	}
-	
-	@Test
-	public void returnParsedArray_GetFileNames_HappyPath() throws ParserConfigurationException, SAXException, IOException {
-		
-		setUpDocument();
-		
+	// All content stored in output array should be convertible to Strings
+	public void returnParsedArray_OutputArrayFormat_ContainsOnlystrings() {
 		try {
-			testOutputArray = gitInspectorParser.returnParsedArray("C:/Git/cs410/src/Test/TestGitInspectorOutput.xml");
+			testOutputArray = gitInspectorParser.returnParsedArray(filePath);
 			Assert.assertNotNull(testOutputArray);
+
+			for (Object[] arrayElement : testOutputArray) {
+				arrayElement.toString();
+
+				// no exception thrown
+				ForceAssertSuccess();
+			}
+		} catch (Exception e) {
+			// should not enter here if this test case is successful
+			ForceAssertFail();
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	// GetAuthors method correctly grabs all authors listed in the XML.
+	// Expected number of authors is 8.
+	public void returnParsedArray_GetAuthors_HappyPath()
+			throws ParserConfigurationException, SAXException, IOException {
+		setUpDocument();
+		setUpExpectedData();
+
+		TreeMap<String, Double> AuthorTable = new TreeMap<String, Double>();
+		AuthorTable = gitInspectorParser.getAuthors(testDoc, testXPath);
+
+		Assert.assertEquals(AuthorTable.size(), 8);
+
+		for (String authorName : expectedAllAuthorNames) {
+			Assert.assertTrue(AuthorTable.containsKey(authorName));
+		}
+	}
+
+	@Test
+	// GetAuthors method correctly grabs all top 6 authors listed in the XML.
+	// Expected number of authors is 6.
+	public void returnParsedArray_GetTop6Authors_HappyPath()
+			throws ParserConfigurationException, SAXException, IOException {
+		setUpDocument();
+		setUpExpectedData();
+
+		ArrayList<String> Top6AuthorList = new ArrayList<String>();
+		Top6AuthorList = gitInspectorParser.getTop6Authors(testDoc, testXPath);
+
+		Assert.assertEquals(Top6AuthorList.size(), 6);
+
+		for (String authorName : expectedTop6AuthorNames) {
+			Assert.assertTrue(Top6AuthorList.contains(authorName));
+		}
+	}
+
+	@Test
+	public void returnParsedArray_GetFileNames_HappyPath()
+			throws ParserConfigurationException, SAXException, IOException {
+
+		setUpDocument();
+
+		List<String> allFileNames = null;
+
+		try {
+			allFileNames = gitInspectorParser.getFileNames(testDoc, testXPath);
+			Assert.assertNotNull(allFileNames);
+
+			Assert.assertEquals(expectedFilePathsNum, allFileNames.size());
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private void setUpDocument() throws ParserConfigurationException, SAXException, IOException {
-		
+
+	private void setUpDocument() throws ParserConfigurationException,
+			SAXException, IOException {
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder builder = null;
+		DocumentBuilder builder = null;
 
 		builder = factory.newDocumentBuilder();
-			
+
 		XPathFactory xpathfactory = XPathFactory.newInstance();
 
 		testDoc = builder.parse(filePath);
 		testXPath = xpathfactory.newXPath();
-		    
+	}
+
+	// helper method to force assert to true. Always evaluates to true
+	private void ForceAssertSuccess() {
+		Assert.assertTrue(1 == 1);
+	}
+
+	// helper method to force assert to false. Always evaluates to false
+	private void ForceAssertFail() {
+		Assert.assertTrue(1 == 2);
+	}
+
+	private void setUpExpectedData() {
+		// Set up all 8 author names in XML test data
+		expectedAllAuthorNames.add("Thomas Han");
+		expectedAllAuthorNames.add("Albert Einstein");
+		expectedAllAuthorNames.add("Slacker Slackerson");
+		expectedAllAuthorNames.add("Michael Bay");
+		expectedAllAuthorNames.add("Hello World");
+		expectedAllAuthorNames.add("Peter Parker");
+		expectedAllAuthorNames.add("Superstar Contributor");
+		expectedAllAuthorNames.add("Average Joe");
+
+		// Set up top 6 author names in XML test data
+		expectedTop6AuthorNames.add("Albert Einstein");
+		expectedTop6AuthorNames.add("Slacker Slackerson");
+		expectedTop6AuthorNames.add("Michael Bay");
+		expectedTop6AuthorNames.add("Hello World");
+		expectedTop6AuthorNames.add("Peter Parker");
+		expectedTop6AuthorNames.add("Superstar Contributor");
 	}
 }
-
--- src/cs410/gitInspectorParser.java
+++ src/cs410/gitInspectorParser.java
@@ -22,14 +22,13 @@ import javax.xml.xpath.XPathFactory;
 import org.xml.sax.SAXException;
 
 public class gitInspectorParser {
-	
+
 	public static int maxAuthorNum = 6;
 	public static int maxFileNum = 6;
-	
-	static Object[][] outputArray = new Object[maxFileNum][maxAuthorNum+1];	
 
-	public static void main(String [ ] args)
-	{
+	static Object[][] outputArray = new Object[maxFileNum][maxAuthorNum + 1];
+
+	public static void main(String[] args) {
 		try {
 			Object[][] helloArray = returnParsedArray("C:/Git/cs410/bin/cs410/GitInspectorJenkins.xml");
 		} catch (Exception e) {
@@ -44,162 +43,177 @@ public class gitInspectorParser {
 	 * @param xpath
 	 * @return
 	 */
-    public static TreeMap<String, Double> getAuthors(Document doc, XPath xpath) {
-        
-    	TreeMap<String, Double> AuthorTable = new TreeMap<String, Double>();
-    	
-        try {
-	        XPathExpression exprAuthor = xpath.compile("/gitinspector/responsibilities/authors/author/name/text()");
-	        Object resultAuthor = (NodeList) exprAuthor.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesAuthor = (NodeList) resultAuthor;
-	        
-	        XPathExpression exprRow = xpath.compile("/gitinspector/responsibilities/authors/author/files/file/rows/text()");
-	        Object resultRow = (NodeList) exprRow.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesRow = (NodeList) resultRow;
-	        
-	        for (int i = 0; i < nodesAuthor.getLength(); i++) {
-	        	AuthorTable.put(nodesAuthor.item(i).getNodeValue(), Double.valueOf(nodesRow.item(i).getNodeValue()));
-	        }
-	        
-	        for (String key : AuthorTable.keySet()) {
-	            //System.out.println(key + ":" + AuthorTable.get(key));
-	        }
-	        
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        
-        return AuthorTable;
-    }
-   
-    /**
-     * 
-     * @param doc
-     * @param xpath
-     * @return
-     */
-    public static ArrayList<String> getTop6Authors(Document doc, XPath xpath) {
-        
-    	ArrayList<String> top6Authors = new ArrayList<String>();
-    	
-    	TreeMap<Double, String> AuthorTable = new TreeMap<Double, String>();
-    	
-        try {
-	        XPathExpression exprAuthor = xpath.compile("/gitinspector/blame/authors/author/name/text()");
-	        Object resultAuthor = (NodeList) exprAuthor.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesAuthor = (NodeList) resultAuthor;
-	        
-	        XPathExpression exprRow = xpath.compile("/gitinspector/blame/authors/author/rows/text()");
-	        Object resultRow = (NodeList) exprRow.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesRow = (NodeList) resultRow;
-	        
-	        for (int i = 0; i < nodesAuthor.getLength(); i++) {
-	        	AuthorTable.put(Double.valueOf(nodesRow.item(i).getNodeValue()), nodesAuthor.item(i).getNodeValue());
-	        }
-	       
-	        /*
-	        System.out.println("...........................");
-	        for (Double key : AuthorTable.keySet()) {
-	            System.out.println(key + ":" + AuthorTable.get(key));
-	        }
-	        */
-	        int size = AuthorTable.size();
-	        /*
-	        System.out.println(size);
-	        System.out.println("HI" + (String) AuthorTable.values().toArray()[size-1]); 
-	        */
-	        
-	        for (int i=1; i<7; i++)
-	        {
-	        	top6Authors.add((String) AuthorTable.values().toArray()[size-i]);
-	        }
-	        
+	public static TreeMap<String, Double> getAuthors(Document doc, XPath xpath) {
 
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        
-        return top6Authors;
-    }
-    
-    /**
-     * 
-     * @param doc
-     * @param xpath
-     * @return
-     */
-    public static List<String> getFileNames(Document doc, XPath xpath) {
-        List<String> list = new ArrayList<>();
-        try {
-        	
-	        XPathExpression expr = xpath.compile("/gitinspector/responsibilities/authors/author/files/file/name/text()");
-	        Object result = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodes = (NodeList) result;
-	        
-	        //System.out.println(nodes.getLength());
-	        for (int i = 0; i < nodes.getLength(); i++) {
-	        	list.add(nodes.item(i).getNodeValue());
-	            //System.out.println(nodes.item(i).getNodeValue());
-	        }
-	        
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        
-        return list;
-    }
-    
-    /**
-     * 
-     * @param doc
-     * @param xpath
-     * @param topSixAuthorsList
-     * @return
-     */
-    public static List<String> getTop6FileNamesForTopAuthors(Document doc, XPath xpath, ArrayList<String> topSixAuthorsList) {
-        
-    	List<String> list = new ArrayList<>();
-        try {
-        	
-        	XPathExpression exprName = xpath.compile("/gitinspector/responsibilities/authors/author/name/text()");
-	        Object resultName = (NodeList) exprName.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesName = (NodeList) resultName;
-	       
-	        XPathExpression exprFile = xpath.compile("/gitinspector/responsibilities/authors/author/files/file/name/text()");
-	        Object resultFile = (NodeList) exprFile.evaluate(doc, XPathConstants.NODESET);
-	        NodeList nodesFile = (NodeList) resultFile;
-	        
-	        /*
-	        System.out.println("******************************");
-	        System.out.println(nodesName.getLength());
-	        */
-	        for (int j= 0; j < topSixAuthorsList.size(); j++) {
-		        for (int i = 0; i < nodesName.getLength(); i++) {
-		        	String ii = nodesName.item(i).getNodeValue();
-		        	String jj = topSixAuthorsList.get(j);
-		        	
-		        	if (ii.equals(jj))
-		        	{
-		        		list.add(ii);
-		        		list.add(nodesFile.item(i).getNodeValue());		
-		        		list.add(nodesFile.item(i+1).getNodeValue());		
-		        		list.add(nodesFile.item(i+2).getNodeValue());		
-		        		list.add(nodesFile.item(i+3).getNodeValue());		
-		        		list.add(nodesFile.item(i+4).getNodeValue());		
-		        		list.add(nodesFile.item(i+5).getNodeValue());		
-		        	}
-		        }        	
-	        }			
-	        
-	        //for ( String listContent : list)
-	        	//System.out.println(listContent);
-	        
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        
-        return list;
-    }
+		TreeMap<String, Double> AuthorTable = new TreeMap<String, Double>();
+
+		try {
+			XPathExpression exprAuthor = xpath
+					.compile("/gitinspector/responsibilities/authors/author/name/text()");
+			Object resultAuthor = (NodeList) exprAuthor.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesAuthor = (NodeList) resultAuthor;
+
+			XPathExpression exprRow = xpath
+					.compile("/gitinspector/responsibilities/authors/author/files/file/rows/text()");
+			Object resultRow = (NodeList) exprRow.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesRow = (NodeList) resultRow;
+
+			for (int i = 0; i < nodesAuthor.getLength(); i++) {
+				AuthorTable.put(nodesAuthor.item(i).getNodeValue(),
+						Double.valueOf(nodesRow.item(i).getNodeValue()));
+			}
+
+			for (String key : AuthorTable.keySet()) {
+				// System.out.println(key + ":" + AuthorTable.get(key));
+			}
+
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+
+		return AuthorTable;
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param xpath
+	 * @return
+	 */
+	public static ArrayList<String> getTop6Authors(Document doc, XPath xpath) {
+
+		ArrayList<String> top6Authors = new ArrayList<String>();
+
+		TreeMap<Double, String> AuthorTable = new TreeMap<Double, String>();
+
+		try {
+			XPathExpression exprAuthor = xpath
+					.compile("/gitinspector/blame/authors/author/name/text()");
+			Object resultAuthor = (NodeList) exprAuthor.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesAuthor = (NodeList) resultAuthor;
+
+			XPathExpression exprRow = xpath
+					.compile("/gitinspector/blame/authors/author/rows/text()");
+			Object resultRow = (NodeList) exprRow.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesRow = (NodeList) resultRow;
+
+			for (int i = 0; i < nodesAuthor.getLength(); i++) {
+				AuthorTable.put(
+						Double.valueOf(nodesRow.item(i).getNodeValue()),
+						nodesAuthor.item(i).getNodeValue());
+			}
+
+			/*
+			 * System.out.println("..........................."); for (Double
+			 * key : AuthorTable.keySet()) { System.out.println(key + ":" +
+			 * AuthorTable.get(key)); }
+			 */
+			int size = AuthorTable.size();
+			/*
+			 * System.out.println(size); System.out.println("HI" + (String)
+			 * AuthorTable.values().toArray()[size-1]);
+			 */
+
+			for (int i = 1; i < 7; i++) {
+				top6Authors.add((String) AuthorTable.values().toArray()[size
+						- i]);
+			}
+
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+
+		return top6Authors;
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param xpath
+	 * @return
+	 */
+	public static List<String> getFileNames(Document doc, XPath xpath) {
+		List<String> list = new ArrayList<>();
+		try {
+
+			XPathExpression expr = xpath
+					.compile("/gitinspector/responsibilities/authors/author/files/file/name/text()");
+			Object result = (NodeList) expr.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodes = (NodeList) result;
+
+			// System.out.println(nodes.getLength());
+			for (int i = 0; i < nodes.getLength(); i++) {
+				list.add(nodes.item(i).getNodeValue());
+				// System.out.println(nodes.item(i).getNodeValue());
+			}
+
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param xpath
+	 * @param topSixAuthorsList
+	 * @return
+	 */
+	public static List<String> getTop6FileNamesForTopAuthors(Document doc,
+			XPath xpath, ArrayList<String> topSixAuthorsList) {
+
+		List<String> list = new ArrayList<>();
+		try {
+
+			XPathExpression exprName = xpath
+					.compile("/gitinspector/responsibilities/authors/author/name/text()");
+			Object resultName = (NodeList) exprName.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesName = (NodeList) resultName;
+
+			XPathExpression exprFile = xpath
+					.compile("/gitinspector/responsibilities/authors/author/files/file/name/text()");
+			Object resultFile = (NodeList) exprFile.evaluate(doc,
+					XPathConstants.NODESET);
+			NodeList nodesFile = (NodeList) resultFile;
+
+			/*
+			 * System.out.println("******************************");
+			 * System.out.println(nodesName.getLength());
+			 */
+			for (int j = 0; j < topSixAuthorsList.size(); j++) {
+				for (int i = 0; i < nodesName.getLength(); i++) {
+					String ii = nodesName.item(i).getNodeValue();
+					String jj = topSixAuthorsList.get(j);
+
+					if (ii.equals(jj)) {
+						list.add(ii);
+						list.add(nodesFile.item(i).getNodeValue());
+						list.add(nodesFile.item(i + 1).getNodeValue());
+						list.add(nodesFile.item(i + 2).getNodeValue());
+						list.add(nodesFile.item(i + 3).getNodeValue());
+						list.add(nodesFile.item(i + 4).getNodeValue());
+						list.add(nodesFile.item(i + 5).getNodeValue());
+					}
+				}
+			}
+
+			// for ( String listContent : list)
+			// System.out.println(listContent);
+
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 
 	/**
 	 * 
@@ -207,57 +221,57 @@ public class gitInspectorParser {
 	 * @return
 	 * @throws Exception
 	 */
-	public static Object[][] returnParsedArray(String filepath) throws Exception {	
-		
-	       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	        factory.setNamespaceAware(true);
-	        DocumentBuilder builder = null;
-	        
-	        try {
-				builder = factory.newDocumentBuilder();
+	public static Object[][] returnParsedArray(String filepath)
+			throws Exception {
 
-		        Document doc = builder.parse(filepath);
-		        
-		        XPathFactory xpathfactory = XPathFactory.newInstance();
-		        XPath xpath = xpathfactory.newXPath();
-		        
-		        TreeMap<String, Double> authorTable = new TreeMap<String, Double>();
-		        List<String> fileNameTable = getFileNames(doc, xpath);
-		        
-		        ArrayList<String> top6Authors = getTop6Authors(doc, xpath);
-				List<String> authorAndFileList = getTop6FileNamesForTopAuthors(doc, xpath, top6Authors);
-				String tempString;
-				int count = 0;
-				
-					for (int temp = 0; temp < maxAuthorNum; temp++) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder = null;
 
-						for (int i=0; i < maxFileNum+1; i++)
-						{
-							
-							
-							outputArray[temp][i] = authorAndFileList.get(count);
-							System.out.println(temp + ":" + ":"+ i+ outputArray[temp][i]);
-							count++;
-						}
-					}
-			
-	        } catch (ParserConfigurationException | SAXException | IOException e) {
-	        e.printStackTrace();
-	        }
-	
-			//testing
-			for(int i = 0; i < outputArray.length; i++)
-			{
-			    for(int j = 0; j < outputArray[i].length; j++)
-			    {
-			        System.out.print(outputArray[i][j]);
-			        if(j < outputArray[i].length - 1) System.out.print(" ");
-			    }
-			    System.out.println();
+		try {
+			builder = factory.newDocumentBuilder();
+
+			Document doc = builder.parse(filepath);
+
+			XPathFactory xpathfactory = XPathFactory.newInstance();
+			XPath xpath = xpathfactory.newXPath();
+
+			TreeMap<String, Double> authorTable = new TreeMap<String, Double>();
+			List<String> fileNameTable = getFileNames(doc, xpath);
+
+			ArrayList<String> top6Authors = getTop6Authors(doc, xpath);
+			List<String> authorAndFileList = getTop6FileNamesForTopAuthors(doc,
+					xpath, top6Authors);
+			String tempString;
+			int count = 0;
+
+			for (int temp = 0; temp < maxAuthorNum; temp++) {
+
+				for (int i = 0; i < maxFileNum + 1; i++) {
+
+					outputArray[temp][i] = authorAndFileList.get(count);
+					System.out.println(temp + ":" + ":" + i
+							+ outputArray[temp][i]);
+					count++;
+				}
 			}
-			
-			return outputArray;
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
+		// testing
+		for (int i = 0; i < outputArray.length; i++) {
+			for (int j = 0; j < outputArray[i].length; j++) {
+				System.out.print(outputArray[i][j]);
+				if (j < outputArray[i].length - 1)
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+		return outputArray;
 
 	}
-	
+
 }
