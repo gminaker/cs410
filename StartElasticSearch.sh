74ce38e434bd9ad4b0c6ee56630a541ff316bd3b
njw374 <njw374@hotmail.com>
2014-11-20 16:43:05 -0800

    Started writing the BASH file

--- StartElasticSearch.sh
+++ StartElasticSearch.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-dir %~dp0
-set elasticSearchDir %~dp0\codebase\elasticSearch\
-set outputDir %~dp0\gitInspectorOutput\
+basedir=$(pwd)
+codebasename=elasticSearch
+elasticSearchDir=$basedir/codebase/elasticSearch/
+outputDir=$basedir/gitInspectorOutput/
+coberturadir=$basedir/codebase/$codebasename/
+gitinspectordir=$basedir/gitinspector/gitinspector
+
+
+cd $coberturadir
 
 mvn cobertura:cobertura -Dcobertura.report.format=xml
 
-python %dir%\gitinspector\gitinspector\gitinspector.py  -r -f, --file-types=java -F xml %elasticSearchDir% > %outputDir%/output.xml
+python $gitinspectordir/gitinspector.py -x file:test  -r -f, --file-types=java -F xml  > $basedir/gitinspectorOutput/$codebasename.xml
 
 
 
+#<>
+
+#<>
+
+#<>
